6ef7d3892a6d4c7fdfee6462673bf9f71665acbc
adibudithi <adibudithi@gmail.com>
2023-04-26 13:28:17 -0400

    added icon hovers

--- scripts/dk-tickle.js
+++
@@ -1,6 +0,0 @@
-function tickle(image) {
-    image.src = "images/assets/dkstill.png";
-}
-function leave(image) {
-    image.src = "images/assets/dk.gif";
-}
---
+++ scripts/hover.js
@@ -0,0 +1,27 @@
+function tickle(image) {
+    image.src = "images/assets/dk2.png";
+}
+function leave(image) {
+    image.src = "images/assets/dk.gif";
+}
+
+function hoverEmail(image) {
+    image.src = "images/assets/email2.png";
+}
+function leaveEmail(image) {
+    image.src = "images/assets/email.png";
+}
+
+function hoverLinkedIn(image) {
+    image.src = "images/assets/linkedin2.png";
+}
+function leaveLinkedIn(image) {
+    image.src = "images/assets/linkedin.png";
+}
+
+function hoverGitHub(image) {
+    image.src = "images/assets/github2.png";
+}
+function leaveGitHub(image) {
+    image.src = "images/assets/github.png";
+}
